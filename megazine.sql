7b3b690dbbd579885439c185ff43437f857922c2
thanhhai99 <haitran23031999@gmail.com>
2020-05-12 21:37:14 +0700

    2020/05/12 21:37:14 ThanhHai99

--- megazine.sql
+++ megazine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 12, 2020 at 01:13 PM
+-- Generation Time: May 12, 2020 at 04:36 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.30
 
