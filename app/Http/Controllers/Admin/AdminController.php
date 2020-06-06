ae838f71d71cda39a9d0e3243bdae05506e6142b
thanhhai99 <haitran23031999@gmail.com>
2020-06-06 18:16:31 +0700

    2020/06/06 18:16:31 ThanhHai99

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\AdminController as AdminController0;
 
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Auth;
@@ -22,15 +23,16 @@ use Yajra\Datatables\Datatables;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
 
-class AdminController extends Controller
+class AdminController extends AdminController0
 {
   use AuthenticatesUsers;
 
   public function __construct() {
     $this->middleware('auth');
   }
-
+  
   public function getIndex(Request $request) {
+    isAdmin();
     if(Auth::user()->id_role != 2 && Auth::user()->id_role != 1 && Auth::user()->id_role != 0) {
       return redirect("/home")->with("notAdmin", "Not allow.");
     }
